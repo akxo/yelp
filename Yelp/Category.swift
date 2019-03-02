6926d9d3b0312d931dfb42e63bd530f727bfb23f
ak <atrix113@aol.com>
2019-03-01 22:44:10 -0500

    initial brainstorm for the category model object

---
+++ Yelp/Category.swift
@@ -0,0 +1,14 @@
+//
+//  Category.swift
+//  Yelp
+//
+//  Created by Alexander Kerendian on 3/1/19.
+//  Copyright © 2019 Alexander Kerendian. All rights reserved.
+//
+
+import Foundation
+
+struct Category: Decodable {
+    let alias: String
+    let title: String
+}
