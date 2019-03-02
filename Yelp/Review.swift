4f815e09c15aa0db0d53b60d8d34a958d742d4e6
ak <atrix113@aol.com>
2019-03-01 23:04:29 -0500

    initial brainstorm for the review model object

---
+++ Yelp/Review.swift
@@ -0,0 +1,18 @@
+//
+//  Review.swift
+//  Yelp
+//
+//  Created by Alexander Kerendian on 3/1/19.
+//  Copyright © 2019 Alexander Kerendian. All rights reserved.
+//
+
+import Foundation
+
+struct Review: Decodable {
+    let id: String
+    let rating: Int
+    let user: User
+    let text: String
+    let time_created: String
+    let url: String
+}
