00dc606cd4020428a2a881c3daaa245ea2a05606
ak <atrix113@aol.com>
2020-02-28 19:10:22 -0500

    refactors network service

--- Yelp/Networking/NetworkService.swift
+++ Yelp/Networking/NetworkService.swift
@@ -6,37 +6,94 @@
 //  Copyright © 2019 Alexander Kerendian. All rights reserved.
 //
 
-import Foundation
-
-class NetworkService {
+struct NetworkService {
     
-    static let defaultSession = URLSession(configuration: .default)
-    static var dataTask: URLSessionDataTask?
-    
-    private init() {}
-    
-    static func makeRequest<T>(with url: URL, and apiKey: String, for type: T.Type, completion: @escaping (Result<T, Error>) -> ()) where T: Decodable {
-        dataTask?.cancel()
-        var request = URLRequest(url: url)
-        request.addValue("Bearer \(apiKey)", forHTTPHeaderField: "Authorization")
-        dataTask = defaultSession.dataTask(with: request) { (data, response, error) in
-            defer { self.dataTask = nil }
-            if let error = error {
-                completion(.failure(error))
-            } else if let data = data, let response = response as? HTTPURLResponse, response.statusCode == 200 {
+    static func request<R, T>(_ route: R, parsedInto type: T.Type, completion: @escaping (Result<T, Error>) -> ()) where R: Router, T: Decodable {
+        request(route) { result in
+            switch result {
+            case .success(let data):
                 do {
                     let jsonDecoder = JSONDecoder()
                     jsonDecoder.keyDecodingStrategy = .convertFromSnakeCase
                     let result = try jsonDecoder.decode(type, from: data)
                     completion(.success(result))
+                    
                 } catch let jsonError {
                     completion(.failure(jsonError))
-                    return
                 }
-            } else {
-                completion(.failure(YelpError.unspecific))
+            case .failure(let error):
+                completion(.failure(error))
             }
         }
-        dataTask?.resume()
     }
+    
+    private static func request<R>(_ route: R, completion: @escaping (Result<Data, APIError>) -> ()) where R: Router {
+        guard let request = route.urlRequest else {
+            completion(.failure(.noUrlRequest))
+            return
+        }
+        
+        let urlSession = URLSession(configuration: route.urlSessionConfiguration)
+        let dataTask = urlSession.dataTask(with: request) { (data, response, error) in
+            
+            guard let response = response as? HTTPURLResponse else {
+                completion(.failure(.invalidResponse))
+                return
+            }
+            
+            switch handleHTTP(response) {
+            case .success:
+                guard let data = data else {
+                    completion(.failure(.noData))
+                    return
+                }
+                
+                completion(.success(data))
+                
+            case .failure(let error):
+                completion(.failure(error))
+            }
+        }
+        dataTask.resume()
+    }
+    
+    private static func handleHTTP(_ response: HTTPURLResponse) -> HTTPResponse {
+        switch response.statusCode {
+        case 200...299:
+            return .success
+        case 400:
+            return .failure(.badRequest)
+        case 401:
+            return .failure(.unauthorized)
+        case 404:
+            return .failure(.notFound)
+        case 500...504:
+            return .failure(.serverError)
+        default:
+            return .failure(.unknown)
+        }
+    }
+    
+    private enum HTTPResponse {
+        case success
+        case failure(APIError)
+    }
+}
+
+public enum APIError: Error {
+    case noUrlRequest
+    case invalidResponse
+    case noData
+    case unknown
+    
+    case badRequest // 400
+    case unauthorized // 401
+    case notFound // 404
+    case serverError // 500 - 504
+}
+
+protocol Router {
+    
+    var urlRequest: URLRequest? { get }
+    var urlSessionConfiguration: URLSessionConfiguration { get }
 }
